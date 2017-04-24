3c673be7a7f33101c20328eb627a11d0a2091412
Jahvon-DS <tvkids8@aol.com>
2017-04-23 23:34:10 -0400

    2nd commit

--- bubble.java
+++ bubble.java
@@ -1,8 +1,10 @@
 package powerPuff;
 
+import java.util.*;
+
 public class bubble 
 {
-	int[] list = {3,0,5,6,4,7,3};
+/*	int[] list = {3,0,5,6,4,7,3};
 	
 	public int[] bubbleSort(int[] list)
 	{
@@ -29,4 +31,38 @@ public class bubble
 	}
 
 }
+*/
+	
+		   public static void main(String[] args) {
+		       ArrayList<Integer> list = new ArrayList<Integer>();
+		       Random r = new Random();
+		       for(int i=0; i<10; i++){
+		           list.add(r.nextInt(100)+1); //generating random number between 1 and 100
+		       }
+		       System.out.println("Before sorting, list elements are: ");
+		       System.out.println(list);
+		       bubbleSort(list);
+		       System.out.println("After sorting, list elements are: ");
+		       System.out.println(list);
+		      
+		   }
+		   public static void bubbleSort(ArrayList<Integer> list) {
+		  
+		int n = list.size();
+		int temp = 0;
 
+		for(int i=0; i < n; i++){
+		for(int j=1; j < (n-i); j++){
+
+		if(list.get(j-1) > list.get(j)){
+		//swap the elements!
+		temp = list.get(j-1);
+		list.set(j-1 , list.get(j));
+		list.set(j, temp);
+		}
+
+		}
+		}
+
+		}
+		}
